557d450b0ef0efd5ab14ab88008de7e6088e7c67
aadrshkashyp <aadarshkashya51@gmail.com>
2023-08-30 02:35:39 +0530

    all fixed

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const ipcRoutes = require('./routes/api/ipcCodes');
-
+const ipcRoutes = require('./routes/api/ipcCodes'); // Import your IPC routes
 
 const app = express();
-const PORT = process.env.PORT || 2115;
+const PORT = process.env.PORT || 5960;
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
+mongoose.connect("mongodb+srv://ipcrti:LRHyGVtTaDpgU2Rw@ipcrti.jxxfdtg.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  user: "ipcrti", // your MongoDB Atlas username
+  pass: "LRHyGVtTaDpgU2Rw" // your MongoDB Atlas password
+})
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
 app.use(cors());
 app.use(express.json());
+
+// Use your IPC routes
 app.use('/api/ipc', ipcRoutes);
 
-
+// Define the root route handler after your API routes
 app.get('/', (req, res) => {
   res.send("Hello, IPC Code Manager!");
 });
